81523905427db65fc0b8c2fa39f2af21aff11191
aaslanyann <axvan2005@gmail.com>
2023-02-01 17:35:47 +0400

    first commit

---
+++ lesson_1/task1.py
@@ -0,0 +1,65 @@
+# Exercise 2
+# --------------------------
+
+# print("Aghvan Aslanyan")
+
+# --------------------------
+
+
+# Exercise 3
+# ---------------------------
+
+# real_time = 2023
+# my_age = 19
+#
+# print(real_time - my_age)
+
+# ---------------------------
+
+
+# Exercise 4
+# ----------------------------
+
+# full_name = "Aghvan Aslanyan"
+# age = 19
+#
+# print(full_name, age)
+
+# ---------------------------
+
+
+# Exercise 5
+# ---------------------------
+
+# data_type = "menq gitenq pytonic str, int, bool, NoneType, float. " \
+#             "Pythonum ka mi dzev popoxakan haytararelu grum enq " \
+#             "popoxakani anuny ev veragrum enq petqakan arjeqy " \
+#             "orinak  // a = 45 \\\ "
+#
+# print(data_type)
+
+# ---------------------------
+
+
+# Exercise 6
+# ---------------------------
+# stex cuyc enq talis mer anun azganuny
+
+# print("Aghvan Aslanyan")
+
+# ---------------------------
+
+
+# Exercise 7
+# ---------------------------
+
+# x = 16
+# y = 5
+#
+# z = x // y == 3
+#
+# print(z)
+
+# ---------------------------
+
+
