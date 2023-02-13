cdaf245cc60b0eb535c48a44f28665ec7e8387dc
aaslanyann <axvan2005@gmail.com>
2023-02-14 02:01:16 +0400

    lesson_7

---
+++ lesson_7/task7.py
@@ -0,0 +1,98 @@
+
+
+# Exercise 1
+# ---------------
+
+# users = [[]]
+# if users:
+# 	print('users exist')
+# else:
+# 	print('users not found')
+
+"""datark listy bool sarqeluc linuma false bayc datark listi mej ka arjeq dra hamar linuma true"""
+# ---------------
+
+
+
+
+# Exercise 2
+# ---------------
+
+# list_pythonists = ["Bush", "Jarvis", "Oxxi", "Buffon", "Vardan"]
+# user_name = "Aghvan"
+#
+# # 2.1
+# if user_name in list_pythonists :
+#     print(user_name)
+# else:
+#     list_pythonists.append(user_name)
+#     print(user_name, "append")
+
+
+
+# 2.2
+
+# users_length = len(list_pythonists)
+#
+# if users_length < 7 and users_length > 5 :
+#     print(users_length, "Pythonists length")
+# elif users_length < 5 :
+#     print("append new user")
+#     list_pythonists.append("Chelentano")
+# elif users_length > 7 :
+#     print("User removed")
+#     list_pythonists.pop()
+
+
+# ---------------
+
+
+# Exercise 3
+# ---------------
+
+
+# start_int = 2
+#
+# while start_int < 21 :
+#     print(start_int)
+#     start_int += 2
+
+# ---------------
+
+
+
+# Exercise 4
+# ---------------
+
+# start_int = 20
+#
+# while start_int > 1 :
+#     if start_int == 17:
+#         start_int -= 1
+#         continue
+#     print(f"{start_int} -- {start_int ** 2}")
+#     start_int -= 1
+
+
+# ---------------
+
+# Exercise 5
+# ---------------
+
+# start_int = 1
+# divisible_three = 0
+#
+# while start_int < 50 :
+#     if start_int % 3 == 0 :
+#         if divisible_three == 10 :
+#             break
+#         print(start_int)
+#         divisible_three += 1
+#
+#     start_int += 1
+
+# ---------------
+
+
+# Research
+"""Else miji gorcoxutyuny katarvuma en jamanak erb while paymany darnuma false katarvuma mi angam"""
