bfa8bb0b85d60042bcd0e67245fe2987187c2dc0
aaslanyann <axvan2005@gmail.com>
2023-02-21 00:20:13 +0400

    lesson_9

---
+++ lesson_9/task9.py
@@ -0,0 +1,74 @@
+# Exercise 1
+# --------------
+
+# def foo(name,last_name, age):
+#     print(f"Hello my names is {name}, my last name os {last_name}, I am {age} years old")
+#
+# foo("Aghvan", "Aslanyan", 19)
+
+# --------------
+
+
+
+# Exercise 2
+# --------------
+
+# def changeOldChar(text, old_character, new_character) :
+#     if len(old_character) >= 2 or len(new_character) >= 2 :
+#         print("Length characters must be exactly 1")
+#         return
+#     text = list(text)
+#     ind = 0
+#     while ind < len(text):
+#         if text[ind] == old_character:
+#             text[ind] = new_character
+#         ind += 1
+#     return "".join(text)
+#
+# print(changeOldChar("ASs", "s", "as"))
+
+
+# --------------
+
+
+# Exercise 3
+# --------------
+
+# def createTextFile(list_of_texts, file_name = "text.txt") :
+#     with open(file_name, "w") as file :
+#         file.writelines(list_of_texts)
+#
+# createTextFile(["Aghvan ", "Aslanyan"], "name_lastName.txt")
+
+# --------------
+
+
+# Exercise 4
+# --------------
+
+# def isPrime(number):
+#
+#     for elem in range(2,number // 2 + 1):
+#         if number % elem == 0:
+#             print("Not prime")
+#             return
+#     print("Prime")
+#
+# isPrime(12)
+
+# --------------
+
+
+
+# Research
+# ---------
+"""
+Functioni mej grum enq amen koxmic ereq chakertneri nersum orinak
+'''
+ays function stuguma prime te che
+'''
+
+"""
+
+
+# ---------
