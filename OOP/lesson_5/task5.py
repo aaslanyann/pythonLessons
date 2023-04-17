de9e3f68fce7fa9c6f1a28a61b40780914a8da69
aaslanyann <axvan2005@gmail.com>
2023-04-17 20:10:05 +0400

    lesson_5

---
+++ OOP/lesson_5/task5.py
@@ -0,0 +1,106 @@
+# Exercise 1
+# ---------------
+
+"""
+inqy mexanizma vory tuyla talis nor classner sarqel arden exac classneri himman vra.
+jarangutyuny tuyla talis jarangel metodnery ev hatkutyunnery u avelacnel mer metodnery ev hatkutyunnery.
+Orinak menq unenq class Animal, menq karox enq sarqel class Lion vory arden kjarangi Animali hatkutyunery ev
+metodnery. Sa mez tuyla talis avelacnel Lion classin mer unikal hatkutyunnery ev metodnery aranc krknelu Animal
+classi miji cody
+"""
+
+# ---------------
+
+
+
+# Exercise 2
+# ---------------
+
+"""
+Da classa vory uxaki chi kareli stexcel ayl miayn jarangvel urish classnerov.
+Abstract classy gone mi hat abstract method metqa unena vory petqa realizacvac 
+chylini abstract classi mej ayl petqa miayn nkaragri ira interfacy.
+Abstract methody karox e nayev unenal sovorakan methodner vory jarangum e ira child classnery.
+Aravelutyuny ogtvel abstract classneric nranum e vor dranq tuyl en talis voroshel yndhanur
+interface mi xumb classneri hamar ev apahovel inch vor astichani verahskum te inchpes e
+ayd interface realizacvum child classnerum.
+"""
+
+# ---------------
+
+
+
+# Exercise 3
+# ---------------
+# import re
+#
+# class FilterText:
+#
+#
+#     def __init__(self, text):
+#         self.text = text
+#
+#     def __getitem__(self, index):
+#         print(self.text.split(" ")[index])
+#
+#
+#     def __getattr__(self, item):
+#         if item == "name":
+#             print(re.findall(r"[A-Z].[A-Z][a-z]+",self.text))
+#         elif item == "dates":
+#             print(re.findall(r"\b\d{1,2}\.\d{2}\.\d{4}", self.text))
+#         elif item == "phone_number":
+#             print(re.findall(r"[0][0-9]{1,2}[-][0-9]{3}[-][0-9]{3}", self.text))
+#
+#
+#
+#     def __setitem__(self, index, value):
+#         data_list = self.text.split(" ")
+#         data_list[index] = value
+#         self.text = ",".join(data_list)
+#
+# x = FilterText("055-445-789 E.Mask 124.06.2014 093-587-135 Nemo 1.09.1887 E.Auditore 24.17.2020 096-2288-987 R.Rocknrolla")
+#
+# x.name
+# x.dates
+# x.phone_number
+#
+# x[1]
+# x[1:3]
+#
+# x[0] = 'G.Ritchie'
+# print(x.text)
+# ---------------
+
+
+# Research
+# ---------------
+
+# 1
+
+"""
+metod __bool__ yurahatuk metoda vory tuyla talis sahmanel vonc petqa objecty iran pahi
+erb ogtragorcvi logicheski kontextum orinak if-um kam while-um.
+Erb menq ogtagorcum enq object-y if-i mej kanchvuma avtomat __bool__ methody
+ev inch vor veradarcnuma __bool__ metody veradarnuma et objecti texy.
+"""
+
+# 2
+
+"""
+metod __len__-y kanchvuma en jamanak erv len functionin atribut talis enq objecty.
+u mer objecti texy veradarnuma en arjeqy vory vor veradarcrela __len__ metody
+"""
+
+
+# 3
+
+"""
+arevelutyune ktrvi __bool__-in isk ete __bool__ metody chka objectum 
+kkanchvi __len__ metody
+"""
+
+
+# ---------------
+
+
