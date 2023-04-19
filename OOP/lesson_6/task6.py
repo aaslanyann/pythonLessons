7b061eca6f4eeb5648f4b3f57f5c7914c0c762fa
aaslanyann <axvan2005@gmail.com>
2023-04-19 23:56:42 +0400

    lesson_6

---
+++ OOP/lesson_6/task6.py
@@ -0,0 +1,95 @@
+# Exercise 1
+# -----------------
+
+"""
+polymorphism-y mez hnaravorutyun e talis ogtarocel tarber classneri objectner
+yndhanur interfacov aysinqn merody ashxati taber data typeri het.
+Polymorphism-y lucum e ayn xndir vor tarber data typeri hamar chenq
+grum arandzin code.
+Polymorphismi orinak da + (plus) operatorn e vor tveri depqum gumarum e irar
+isk str-neri depqum uxaki kpcnum irar.
+"""
+
+# -----------------
+
+
+
+
+
+
+# Exercise 2
+# -----------------
+
+# class TeslaMotors:
+#     names = ['Model S', 'Roadster', 'Model 3', 'Cybertruck', 'Model Y', 'Model X']
+#     body_types = ['Sedan', 'Coupe', 'Sedan', 'Truck', 'SUVS', 'SUVS']
+#     engine_types = ['Electric', 'Electric', 'Electric', 'Electric', 'Electric']
+#
+#     def __init__(self, order = "names"):
+#         self.order = order
+#         self.start = 0
+#
+#     def __iter__(self):
+#         self.start = 0
+#         return self
+#
+#
+#
+#     def __next__(self):
+#         if self.start >= len(self.get_list()):
+#             raise StopIteration
+#         result = self.get_list()[self.start]
+#         self.start += 1
+#         return result
+#
+#
+#     def get_list(self):
+#         order = self.order
+#         if order == "names":
+#             return self.names
+#         elif order == "body_types":
+#             return self.body_types
+#         elif order == "engine_types":
+#             return self.engine_types
+#         return []
+#
+#     def set_iter_by(self, order):
+#         self.order = order
+#
+#
+# x = TeslaMotors()
+#
+# for el in x:
+#     print(el)
+
+# tesla = TeslaMotors('body_types')
+#
+# print('Coupe' in tesla )  # True
+# print('Model S' in tesla) # False
+#
+# tesla.set_iter_by('names')
+#
+# print('Coupe' in tesla)   # False
+# print('Model S' in tesla )
+
+# -----------------
+
+
+
+# Research
+# -----------------
+
+# 1
+"""
+__call__ metody ashxatuma en jamanak erb objecty kancum enq functioni pes
+"""
+
+# 2
+"""
+erb vor objectnery hamematum es irar het ashxatum en __ls__ (less then) u __gt__ (greater then) metodnery
+"""
+
+
+
+
+# -----------------
