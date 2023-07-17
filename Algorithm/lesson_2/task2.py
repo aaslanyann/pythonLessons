9df74b053bc350519ee23950f7882a8d84c72221
aaslanyann <axvan2005@gmail.com>
2023-07-17 20:05:26 +0400

    algo lesson 2

---
+++ Algorithm/lesson_2/task2.py
@@ -0,0 +1,83 @@
+# Exercise 1
+# --------------------
+
+# 1.1
+"""
+bardutyuny klini O(n)
+"""
+
+# 1.2
+"""
+bardutyuny klini O(n)
+"""
+
+
+# --------------------
+
+
+
+
+# Exercise 2
+# --------------------
+
+
+# 2.2
+
+"""
+function euclid_algorithm(a,b):
+    while b is not equal to 0:
+        a,b = b, a modulo b
+    return a
+"""
+
+
+
+
+
+
+
+
+# 2.3
+# def euclid_algorithm(a, b):
+#     while b != 0:
+#         a, b = b, a % b
+#         print(a , b)
+#     return a
+# a = 84
+# b = 18
+#
+# print(euclid_algorithm(a, b))
+
+
+# --------------------
+
+
+
+
+# Exercise 3
+# --------------------
+
+# def binary_decimal(binar):
+#     digit_list = list(binar)[::-1]
+#     count = 0
+#     result = 0
+#     for item in digit_list:
+#         if item == "1":
+#             result += pow(2, count)
+#         count += 1
+#
+#     return result
+#
+# print(binary_decimal("1111"))
+#
+#
+#
+
+
+
+# --------------------
+
+
+
+
+
