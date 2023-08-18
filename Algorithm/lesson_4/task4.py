acdf5ee753aa8fcd4b0d572d8e8676d20c6bf15a
aaslanyann <axvan2005@gmail.com>
2023-08-18 16:01:22 +0400

    algo lesson_4

---
+++ Algorithm/lesson_4/task4.py
@@ -0,0 +1,81 @@
+# Exercises 1
+#-------------------------
+
+"""
+n^2
+vorovhetev erku loopova ashxatum
+"""
+
+#-------------------------
+
+
+# Exercises 2
+#-------------------------
+
+"""
+mek angam kpoxi
+uxaki 4-y 2-i het kpoxi u verj
+"""
+
+
+
+
+#-------------------------
+
+
+
+# Exercises 3
+#-------------------------
+
+# lst = [4, 2, 6, 7, 7]
+#
+# function bubble_sort(lst):
+#     for cnt in range(1, length(lst)):
+#         for i in range(length(lst) - cnt):
+#             if lst[i] > lst[i + 1]:
+#                 swap(lst[i], lst[i + 1])
+#     return lst
+#
+# print(bubble_sort(lst))
+# print(list(range(1, length(lst))))
+
+#-------------------------
+
+
+
+# Exercises 4
+#-------------------------
+
+
+# lst = [4, 2, 6, 7, 7]
+#
+# # order must be decrease or growth
+# def bubble_sort(lst, order):
+#   for cnt in range(1, len(lst)):
+#     for i in range(len(lst) - cnt):
+#       if order == "decrease":
+#         if lst[i] < lst[i + 1]:
+#           lst[i], lst[i + 1] = lst[i + 1], lst[i]
+#       elif order == "growth":
+#         if lst[i] > lst[i + 1]:
+#           lst[i], lst[i + 1] = lst[i + 1], lst[i]
+#   return lst
+#
+#
+# print(bubble_sort(lst, "decrease"))
+
+
+
+
+
+
+
+
+#-------------------------
+
+
+
+
+
+
+
